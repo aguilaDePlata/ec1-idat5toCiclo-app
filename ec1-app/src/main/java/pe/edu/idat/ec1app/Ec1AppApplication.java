1c69a1578fe37b0b3aacc4526e3707fd3dcade14
aguilaDeOro <jorgeluis_illescas@hotmail.com>
2023-06-16 23:25:04 -0500

    feature: inicializa spring boot

---
+++ ec1-app/src/main/java/pe/edu/idat/ec1app/Ec1AppApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec1app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec1AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec1AppApplication.class, args);
+	}
+
+}
---
+++ ec1-app/src/test/java/pe/edu/idat/ec1app/Ec1AppApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec1app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec1AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
